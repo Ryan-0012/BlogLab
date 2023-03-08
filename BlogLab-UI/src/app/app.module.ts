f6ac567de72fb3597794dbc9cc25f0a82ab36a9e
Ryan-0012 <ryanfreitas1229@gmail.com>
2023-03-08 11:40:45 -0300

    Login component created.

--- BlogLab-UI/src/app/app.module.ts
+++ BlogLab-UI/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
@@ -29,6 +30,7 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -55,6 +57,8 @@ import { ErrorInterceptor } from './interceptors/error.interceptor';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
