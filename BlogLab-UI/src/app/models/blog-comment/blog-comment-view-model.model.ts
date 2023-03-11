6eff8574f1b01cc7dea1f3c0d90a0ae1a3f13ff0
Ryan-0012 <ryanfreitas1229@gmail.com>
2023-03-11 16:35:05 -0300

    Comment components created. blog component edited. Famous and blogs components are created.

--- BlogLab-UI/src/app/models/blog-comment/blog-comment-view-model.model.ts
+++ BlogLab-UI/src/app/models/blog-comment/blog-comment-view-model.model.ts
@@ -1,16 +1,17 @@
 export class BlogCommentViewModel {
     constructor(
-        public parentBlogCommentId: number,
         public blogCommentId: number, 
         public blogId: number,
         public content: string,
-        public username: string,
-        public applicationUserId: number,
-        public publishDate: Date,
-        public updateDate: Date,
+        public username: string | undefined,
+        public applicationUserId: number | null,
+        public publishDate: Date | null,
+        public updateDate: Date | null,
         public isEditable: boolean = false,
         public deleteConfirm: boolean = false,
         public isReplying: boolean = false,
-        public comments: BlogCommentViewModel[]
+        public comments: BlogCommentViewModel[],
+        public parentBlogCommentId?: number | null,
+        
     ){}
 }
